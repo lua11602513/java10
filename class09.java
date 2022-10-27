f1da35f4344b1f1dc423f75ea5972bcdefc8cf89
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-27 23:50:34 +0800

    Add files via upload

---
+++ class09.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package java10;
+class Caaa{
+	private int num;
+	public void display(){
+		System.out.println("printed from Caaa class");
+}
+ }
+ class Cbbb extends Caaa
+ {
+	public void display(){
+		System.out.println("printed from Cbbb class");
+}
+ }
+public class class09{
+	public static void main(String args[]){
+		Caaa bb=new Cbbb();
+		bb.display();
+}
+ }
