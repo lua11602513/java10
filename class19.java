0cee96e2120ea3dc4d274e4418feb9eaabb2f744
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-28 00:52:50 +0800

    Create class19.java

---
+++ class19.java
@@ -0,0 +1,20 @@
+package java10;
+class CRectangle{
+	protected int length;
+	protected int width;
+public CRectangle(int l,int w){
+	length=l;
+	width=w;
+}
+public String toString(){
+	String str="length="+length+", width="+width;
+	str+=", area="+length*width;
+	return str;
+}
+ }
+public class class19{
+	public static void main(String args[]) {
+		CRectangle rect=new CRectangle(2,6);
+		System.out.println(rect);
+}
+} 
