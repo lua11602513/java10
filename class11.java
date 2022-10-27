67bdef4d75eb1abbe159d48772713b3b40eaf088
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-27 23:59:36 +0800

    Add files via upload

---
+++ class11.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package java10;
+
+/**
+ * @author Acer
+ *
+ */
+public class class11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
